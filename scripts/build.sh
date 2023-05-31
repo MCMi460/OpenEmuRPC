526afa34646b3683622c588ad4ea4ff4ab9d4a5e
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-05-31 17:17:23 -0500

    Stop using virtual environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
 cd ..
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install -r requirements.txt py2app
-python build.py py2app -O2
+python3 -m pip install -r requirements.txt py2app
+python3 build.py py2app -O2
 open ./dist
